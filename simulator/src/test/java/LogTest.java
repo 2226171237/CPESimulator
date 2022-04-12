22ac0ec9ecb079e9e6e539d8d5c10b182133d0a1
2226171237 <2226171237@qq.com>
2022-04-12 23:57:41 +0800

    增加ACS主动连接事件处理

--- simulator/src/test/java/LogTest.java
+++ simulator/src/test/java/LogTest.java
@@ -4,5 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 public class LogTest {
     public static void main(String[] args) {
         log.info("ok");
+//        https://zhuanlan.zhihu.com/p/20913727
+//        https://www.cnblogs.com/woshimrf/p/OAuth2.html
+//        https://blog.csdn.net/u010609251/article/details/108030120
     }
 }
